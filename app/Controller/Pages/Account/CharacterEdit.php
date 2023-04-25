6d467328fd1f8bc3157f2b5b9e5cd5481866703a
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2023-04-24 22:18:57 -0700

    Update app/Controller/Pages/Account/CharacterEdit.php

--- app/Controller/Pages/Account/CharacterEdit.php
+++ app/Controller/Pages/Account/CharacterEdit.php
@@ -37,9 +37,9 @@ class CharacterEdit extends Base{
                 $filter_account = 0;
             }
             $filter_outfit = filter_var($postVars['hide_outfit'], FILTER_SANITIZE_NUMBER_INT);
-			if(empty($filter_outfit)){
-				$filter_outfit = 0;
-			}
+            if(empty($filter_outfit)){
+                $filter_outfit = 0;
+            }
             $filter_inventory = filter_var($postVars['hide_inventory'], FILTER_SANITIZE_NUMBER_INT);
 			if(empty($filter_inventory)){
 				$filter_inventory = 0;
